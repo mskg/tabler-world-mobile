ddf4793e368b0697fca6807b7155733e56917d71
Markus Kling <markus.kling@gmail.com>
2019-06-20 12:25:10 +0200

    fix: offline urls for DynamoDB

--- Backend/src/graphql/cache/instance.ts
+++ Backend/src/graphql/cache/instance.ts
@@ -5,10 +5,10 @@ const disableCache = process.env.DISABLE_CACHE === "true";
 export const cacheInstance = disableCache ? new NoCache() : new DynamoDBCache(
     {
         region: process.env.AWS_REGION,
-        // endpoint:
-        //   process.env.IS_OFFLINE === 'true'
-        //   ? 'http://localhost:8000'
-        //   : undefined
+        endpoint:
+            process.env.IS_OFFLINE === 'true'
+                ? 'http://localhost:8000'
+                : undefined
     },
     {
         tableName: process.env.cache_table || 'typescript',
