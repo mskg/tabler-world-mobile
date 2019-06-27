96217f1f23daab332ed676d7a0705dad006f14cf
Markus Kling <markus.kling@gmail.com>
2019-06-27 15:08:03 +0200

    workarround: hide export functionality for android #9

--- App/src/model/Features.ts
+++ App/src/model/Features.ts
@@ -1,13 +1,19 @@
+import { Platform } from 'react-native';
 
 export enum Features {
     ContactSync,
     LocalBirthdayNotifications,
     EncryptedStorage,
     BackgroundFetch,
+    SendToAdressbook,
 }
 
 export function isFeatureEnabled(feature: Features) {
     if (feature === Features.BackgroundFetch) {
+        return true;
+    }
+
+    if (feature === Features.SendToAdressbook && Platform.OS === "ios") {
         return true;
     }
 
