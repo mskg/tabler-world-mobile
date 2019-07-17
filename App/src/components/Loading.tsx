6119ffd08ab1653870e8f9f5d775581fac50771d
Markus Kling <markus.kling@gmail.com>
2019-07-17 11:08:28 +0200

    Workarround issue https://github.com/expo/expo/issues/2078

--- App/src/components/Loading.tsx
+++ App/src/components/Loading.tsx
@@ -1,6 +1,6 @@
 import { SplashScreen } from "expo";
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StatusBar, StyleSheet, View } from "react-native";
 import { ActivityIndicator } from 'react-native-paper';
 import Assets from "../Assets";
 import { ___DONT_USE_ME_DIRECTLY___COLOR_ACCENT } from '../theme/colors';
@@ -10,6 +10,7 @@ export default class Loading extends React.PureComponent {
     render() {
         return (
             <View style={{ flex: 1 }}>
+                <StatusBar barStyle="dark-content" backgroundColor="white" translucent={true} />
                 <Image
                     style={styles.image}
                     source={Assets.images.splash}
