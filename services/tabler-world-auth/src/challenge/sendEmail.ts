7ea9c4fc5ca83740b9278a64e3a9d65115f50cdd
Markus Kling <markus.kling@gmail.com>
2019-08-29 17:12:04 +0200

    Add reply-to, resolves #64

--- services/tabler-world-auth/src/challenge/sendEmail.ts
+++ services/tabler-world-auth/src/challenge/sendEmail.ts
@@ -3,16 +3,19 @@ import { ses } from './create';
 
 export async function sendEmail(emailAddress: string, secretLoginCode: string) {
     console.log("sending email");
-    var filename = require("./mail.html");
-    var content = readFileSync(filename, 'utf8');
+    const filename = require("./mail.html");
+    const content = readFileSync(filename, 'utf8');
 
     const maskEMail = emailAddress.replace(/(..)[^@]+@(\d)\d*-\w+\.roundtable\.world/ig, "$1****@$2****.roundtable.world");
-    var replaceContent = content
+    const replaceContent = content
         .replace(/##MAIL##/g, maskEMail)
         .replace(/##CODE##/g, secretLoginCode);
 
+    const suffix = process.env.STAGE === "prod" ? "" : ` (${process.env.STAGE})`;
+
     const params: AWS.SES.SendEmailRequest = {
         Destination: { ToAddresses: [emailAddress] },
+        ReplyToAddresses: [process.env.SES_REPLY_ADDRESS || 'support@round-table.de'],
         Message: {
             Body: {
                 Html: {
@@ -22,11 +25,11 @@ export async function sendEmail(emailAddress: string, secretLoginCode: string) {
             },
             Subject: {
                 Charset: 'UTF-8',
-                Data: 'TABLER.WORLD Mobile Security Code'
+                Data: 'TABLER.WORLD Mobile Security Code' + suffix
             }
         },
         Source: process.env.SES_FROM_ADDRESS as string
     };
-    
+
     await ses.sendEmail(params).promise();
 }
