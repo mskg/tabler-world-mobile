9550eee3ba1b80998cb93974a925c3414377867f
Markus Kling <markus.kling@gmail.com>
2019-10-09 12:05:03 +0200

    Add 'all' permission

--- services/tabler-world-api/src/graphql/privacy/DBLevels.ts
+++ services/tabler-world-api/src/graphql/privacy/DBLevels.ts
@@ -1,5 +1,5 @@
-export const PUBLIC = '__public__';
-
+export const PUBLIC = 'public';
 export const ASSOCIATION = 'association';
 export const CLUB = 'club';
 export const PRIVATE = 'private';
+export const ALL = 'all';
