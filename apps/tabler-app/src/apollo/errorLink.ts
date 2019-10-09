77deba5cfb45c49a504ba6795f6b588837858090
Markus Kling <markus.kling@gmail.com>
2019-10-10 01:09:47 +0200

    Better error output for GQL errors

--- apps/tabler-app/src/apollo/errorLink.ts
+++ apps/tabler-app/src/apollo/errorLink.ts
@@ -1,8 +1,20 @@
 import { onError } from 'apollo-link-error';
+import { DocumentNode } from 'graphql';
 import { logger } from './logger';
 
+function getGqlString(doc: DocumentNode) {
+    return doc.loc && doc.loc.source.body;
+}
+
 export const errorLink = onError(({ networkError, graphQLErrors, response, operation }) => {
-    logger.log('Response', response, 'Operation', operation);
+    const { operationName, query, variables } = operation;
+
+    logger.log(
+        'Failed to execute operation', operationName,
+        'query', getGqlString(query),
+        'variables', variables,
+        'Response', response,
+    );
 
     if (networkError) {
         logger.error(networkError);
