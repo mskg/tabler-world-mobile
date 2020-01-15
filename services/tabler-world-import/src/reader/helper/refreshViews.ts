1bac2a3b0a993a47cea2ce7deff392ac4bff8267
Markus Kling <markus.kling@gmail.com>
2020-01-15 15:21:47 +0100

    Fixed order of refresh

--- services/tabler-world-import/src/reader/helper/refreshViews.ts
+++ services/tabler-world-import/src/reader/helper/refreshViews.ts
@@ -7,10 +7,16 @@ export const refreshViews = async (client: IDataService) => {
     console.log('Updating views');
 
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_groups');
+
+    // dependent on groups
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_tabler_roles');
+
+    // dependent on groups, and roles
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY profiles');
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY profiles_privacysettings');
+
+    // dependent data
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_clubs');
-    await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_associations');
     await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_areas');
+    await client.query('REFRESH MATERIALIZED VIEW CONCURRENTLY structure_associations');
 };
