a8fae89a78ed8cb9f472ffcfadbf8aab58dcd2ef
Markus Kling <markus.kling@gmail.com>
2020-01-29 09:30:59 +0100

    Add WS Api to configuration

--- apps/tabler-app/src/helper/getConfigValue.ts
+++ apps/tabler-app/src/helper/getConfigValue.ts
@@ -45,10 +45,7 @@ export function getConfigValue<T extends ConfigTypes = string>(key: Key): T {
         } if (Platform.OS === 'ios') {
             return 'ws://localhost:3001' as T;
         }
-    } else if (key === 'ws-api') {
-        return Constants.manifest.extra.api.replace('https', 'wss').replace('api', 'ws-api');
     }
-
 
     return Constants.manifest.extra[key];
 }
