86143110e3338e550ddf609191157ed3366fbb71
Markus Kling <markus.kling@gmail.com>
2020-02-28 19:54:23 +0100

    Remove keepalive from XRAY, now default

--- packages/tabler-world-aws/src/xray/aws.ts
+++ packages/tabler-world-aws/src/xray/aws.ts
@@ -1,16 +1,7 @@
 import * as AWS from 'aws-sdk';
-import https, { Agent } from 'https';
+import https from 'https';
 import * as pg from 'pg';
 import { EXECUTING_OFFLINE } from '../isServerlessOffline';
-
-// we connect to local HTTP in case of serverless offline
-const agent = EXECUTING_OFFLINE
-    ? undefined
-    : new Agent({
-        keepAlive: true,
-        rejectUnauthorized: true,
-        maxSockets: Number.POSITIVE_INFINITY,
-    });
 
 let xAWS: typeof AWS;
 let xPG: typeof pg;
@@ -26,22 +17,14 @@ if (EXECUTING_OFFLINE || process.env.XRAY_DISABLED === 'true') {
     xHttps = require('https');
 } else {
     XRAY = require('aws-xray-sdk');
-    XRAY.setStreamingThreshold(parseInt(process.env.XRAY_STREAMING_THRESHOLD || '0', 10));
-
-    isXrayEnabled = true;
-
     xAWS = XRAY.captureAWS(require('aws-sdk'));
     xPG = XRAY.capturePostgres(require('pg'));
     xHttps = XRAY.captureHTTPs(require('https'));
 
+    XRAY.setStreamingThreshold(parseInt(process.env.XRAY_STREAMING_THRESHOLD || '0', 10));
     XRAY.capturePromise();
+    isXrayEnabled = true;
 }
-
-xAWS.config.update({
-    httpOptions: {
-        agent,
-    },
-});
 
 export { xAWS, xPG, isXrayEnabled, XRAY, xHttps };
 
