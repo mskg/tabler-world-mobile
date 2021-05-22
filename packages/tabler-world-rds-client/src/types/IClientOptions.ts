c2bff963a9b6c430333ed4288e910143a981b730
Markus Kling <markus.kling@gmail.com>
2021-05-22 15:56:20 +0200

    Rename config variable sqlLogLevel

--- packages/tabler-world-rds-client/src/types/IClientOptions.ts
+++ packages/tabler-world-rds-client/src/types/IClientOptions.ts
@@ -4,5 +4,5 @@ import { ILogger, Metric } from '@mskg/tabler-world-common';
 export interface IClientOptions {
     logger: ILogger;
     metrics?: Metric;
-    logLevel?: 'error' | 'debug';
+    sqlLogLevel?: 'error' | 'debug';
 }
